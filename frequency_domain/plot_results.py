fee6cdee3791c6c19c5837fc75bd68751bd2cc0f
lucarebuffi <lrebuffi@anl.gov>
2020-06-03 18:33:18 -0500

    BM simulation

--- frequency_domain/plot_results.py
+++ frequency_domain/plot_results.py
@@ -1,15 +1,14 @@
 import os, numpy
-
-from silx.gui.plot import Plot2D, PlotWindow
+from matplotlib import pyplot as plt
+from silx.gui.plot import Plot2D
 from core.default_values import base_output_dir
 
 def plot_spectrum(spectrum):
-    plot = PlotWindow(fit=True)
-    plot.addCurve(spectrum[:, 0], spectrum[:, 1])
-    plot.setGraphTitle("Spectrum used for Power Calculation")
-    plot.setGraphXLabel("Energy [eV]")
-    plot.setGraphYLabel("Spectral Flux [ph/s/0.1%BW]")
-    plot.show()
+    plt.plot(spectrum[:, 0], spectrum[:, 1])
+    plt.xlabel("Energy [eV]")
+    plt.ylabel("Spectral Flux [ph/s/0.1%BW]")
+    plt.title("Spectrum used for Power Calculation")
+    plt.show()
 
 def plot_power_density(x_coord, y_coord, power_density):
     origin = (x_coord[0], y_coord[0])
@@ -54,12 +53,12 @@ def plot_data_files(outdir=None):
     outdir = os.path.join(base_output_dir, "frequency_domain") if outdir is None else outdir
     if not os.path.exists(outdir): return
 
-    #spectrum            = numpy.loadtxt(os.path.join(outdir, "Spectrum_at_focus.txt"))
+    spectrum            = numpy.loadtxt(os.path.join(outdir, "Spectrum_at_focus.txt"))
     plot_coordinates_x  = numpy.loadtxt(os.path.join(outdir, "Power_Density_at_Focus_coord_x.txt"))
     plot_coordinates_y  = numpy.loadtxt(os.path.join(outdir, "Power_Density_at_Focus_coord_y.txt"))
     total_power_density = numpy.loadtxt(os.path.join(outdir, "Power_Density_at_Focus.txt"))
 
-    #plot_spectrum(spectrum)
+    plot_spectrum(spectrum)
     plot_power_density(plot_coordinates_x, plot_coordinates_y, total_power_density)
 
 
